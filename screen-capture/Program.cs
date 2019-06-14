3b3397ce51ab64f58c7688bdf7c9d1c98347ae54
BlaCkMeloDy <mkbvnh@gmail.com>
2019-06-14 09:01:44 +0700

    no message

---
+++ screen-capture/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Net;
+
+namespace screen_capture
+{
+    class Program
+    {
+        static void Main()
+        {
+            string url = "";
+            //ScreenCapture.CaptureDesktop().Save(@"\screen.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //ScreenCapture.CaptureActiveWindow().Save(@"\window.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            using (var wc = new WebClient())
+            {
+                wc.UploadData(url, "POST", ImageToByte(ScreenCapture.CaptureDesktop()));
+            }
+        }
+
+        public static byte[] ImageToByte(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+    }
+}
---
+++ server/api/models/todoListModel.js
@@ -0,0 +1,9 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+
+})
+
+module.exports = mongoose.model('Tasks', TaskSchema);
